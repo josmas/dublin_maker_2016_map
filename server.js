b460ba9c389627bd8265d81b6c0a601daf18ce54
josmas <josmasflores@gmail.com>
2016-07-13 18:17:46 +0100

    tokenising the POST payload

--- server.js
+++ server.js
@@ -17,9 +17,34 @@ server.route({
   method: ['PUT', 'POST'],
   path: '/maprequest',
   handler: function (request, reply) {
-      console.log(request.payload);
-      //TODO (jos) tokenize and push to DB
-      reply('Done');
+    var lat, lng, name, pic;
+    console.log("payload: " + request.payload);
+    var payload = request.payload;
+    console.log(typeof request.payload);
+
+    // The format of the payload is: { '14 : 34 : jos : female_3.png': '' } BUT...
+
+    // From the phone, it comes in as an object (not locally, but only from App Inventor calls)
+    if (typeof payload === 'object') {
+      lat = Object.keys(payload)[0].split(' : ')[0];
+      lng = Object.keys(payload)[0].split(' : ')[1];
+      name = Object.keys(payload)[0].split(' : ')[2];
+      pic = Object.keys(payload)[0].split(' : ')[3];
+    }
+
+    // Cannot reproduce the object encoding locally so creating an additional parser
+    if (typeof payload === 'string'){
+      payload = payload.split("'")[1];
+      lat = payload.split(' : ')[0];
+      lng = payload.split(' : ')[1];
+      name = payload.split(' : ')[2];
+      pic = payload.split(' : ')[3];
+    }
+
+    //TODO (jos) Push to DB
+
+    console.log(lat + ' '  + lng + ' ' + name + ' ' + pic);
+    reply('Done');
   }
 });
 
