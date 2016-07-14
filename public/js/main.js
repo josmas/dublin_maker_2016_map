f77dd70afa203c338372e9a744ed836b06a7727d
josmas <josmasflores@gmail.com>
2016-07-14 15:08:41 +0100

    adds jquery

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-(function(){
+$(function(){
   console.log('I am being loaded');
 
   // Setting up the map
@@ -8,8 +8,8 @@
   L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
-    id: 'YOUR_ID',
-    accessToken: 'YOUR_ACCESS_TOKEN'
+    id: 'dublin_maker_map_2016',
+    accessToken: 'pk.eyJ1Ijoiam9zbWFzZmxvcmVzIiwiYSI6ImNpcW1ja2kzejAwMWlodG1hbTFrc2Iwdm8ifQ._jzG76JQzFERSM1V2RhqLA'
   }).addTo(mymap);
 
-}());
+});
