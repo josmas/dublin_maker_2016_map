2c66ef14ba4e25cc3923837aabc658b794d8a96c
josmas <josmasflores@gmail.com>
2016-07-14 11:40:16 +0100

    inserts in the DB

--- db_calls.js
+++ db_calls.js
@@ -7,13 +7,29 @@ function setConnection(conn) {
   connection = conn;
 }
 
+/**
+* This function expects that the database 'jos' and table 'markers' EXIST.
+*/
 function storeInDB(marker, cb) {
   if (connection) console.log('good to go with the DB');
   else return cb(new Error('DB connection not available.'));
 
   console.log('The marker is: ' + JSON.stringify(marker));
-  //TODO (jos) cb(err) when adding DB code
-  cb(null, 'all seems good');
+  if (!marker.lat || !marker.lng || !marker.name || !marker.pic) {
+    return cb(new Error('Not enough info to store the Marker'));
+  }
+
+  r.db('jos').table('markers').insert([
+    marker
+  ]).run(connection, function(err, result) {
+    if (err) {
+      console.log(err);
+      return cb(err);
+    }
+    console.log(JSON.stringify(result, null, 2));
+    cb(null, 'all seems good');
+  })
+
 }
 
 module.exports = {
--- test/integration/db_tests.js
+++ test/integration/db_tests.js
@@ -4,6 +4,13 @@ const db = require('../../db_calls');
 const expect = require('chai').expect;
 const r = require('rethinkdb');
 
+const firstMarker = {
+  lat: 53.384635499999995,
+  lng: -6.602360699999963,
+  name: 'jos',
+  pic: 'male_1.jpg'
+};
+
 describe('A locally availabe, fully functioning rethinkDB database', function() {
 
   it('should have a null connection when not configured', function (done) {
@@ -17,8 +24,31 @@ describe('A locally availabe, fully functioning rethinkDB database', function() 
     r.connect( {host: '192.168.99.100', port: 28015}, function(err, conn) {
       if (err) throw err;
       db.setConnection(conn);
-      db.storeInDB({}, function(err, result){
+      db.storeInDB(firstMarker, function(err, result){
         expect(err).to.be.null;
+        done();
+      })
+    });
+  });
+
+  it('should error when there is not enough information for a marker', function (done) {
+    r.connect( {host: '192.168.99.100', port: 28015}, function(err, conn) {
+      if (err) throw err;
+      db.setConnection(conn);
+      db.storeInDB({}, function(err, result){
+        expect(err).not.to.be.null;
+        done();
+      })
+    });
+  });
+
+  it('should store a record with a real connection configured', function (done) {
+    r.connect( {host: '192.168.99.100', port: 28015}, function(err, conn) {
+      if (err) throw err;
+      db.setConnection(conn);
+      db.storeInDB(firstMarker, function(err, result){
+        expect(err).to.be.null;
+        expect(result).not.to.be.null;
         done();
       })
     });
